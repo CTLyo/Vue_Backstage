e4edc17188e837eaf766d2f041eb1f72ed181e9f
CityHunter <saiyaba744@gmail.com>
2022-08-09 21:39:36 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-button type="warning" round @click="onLogout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods:{
+    onLogout(){
+        // 清空token
+        window.sessionStorage.clear()
+        // 跳转到登录页面
+        this.$router.push('/login')
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入全局样式表
+import '@/assets/CSS/global.css'
+// 导入图标
+import '@/assets/fonts/iconfont.css'
+// 导入axios
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
 
 Vue.config.productionTip = false
 
